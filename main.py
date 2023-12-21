c9eae1f63922f2fd502db482cae4bdb04ae2d9a1
Prasham <theprasham@gmail.com>
2023-12-20 17:17:44 -0800

    made code for updating balence in sqlite

--- main.py
+++ main.py
@@ -14,12 +14,21 @@ homepage = Frame(root, width=50, height=150)
 signin.grid(row=0,column=0)
 
 def loadLogin():
-    signin.forget()
+    signin.grid_forget()
     login.grid(row=0, column=0)
 def LoginSucessful():
-    global login
-    login.forget()
+    login.grid_forget()
     homepage.grid(row=0,column=0)
+def withdraw():
+    money = int(amountEntry.get())
+    UserBankObject.balance = UserBankObject.balance - money
+    c.execute(f"UPDATE bank_info SET balance = {UserBankObject.balance} WHERE username = '{UserBankObject.username}'")
+    conn.commit()
+
+def deposit():
+    money = int(amountEntry.get())
+    UserBankObject.balance = UserBankObject.balance + money
+    balanceDisplay.config(text=f"balance: {UserBankObject.balance}")
 
 root.title("SIGN IN")
 new_user = Button(signin, text="New User", font=("Arial", 20, "bold"))
@@ -41,7 +50,13 @@ def enterButtonClicked():
     row = c.fetchone()
     print(row)
     global UserBankObject 
-    UserBankObject = bank_object.bank(row[0], row[1], row[2], row[3], row[4])
+    UserBankObject.accountnum = row[0]
+    UserBankObject.username = row[1]
+    UserBankObject.name = row[2]
+    UserBankObject.password = row[3]
+    UserBankObject.balance = row[4]
+    nameDisplay.config(text=f"Hello {UserBankObject.name}                    ")
+    balanceDisplay.config(text=f"balance: {UserBankObject.balance}")
     LoginSucessful()
 
 enterButton = Button(login, text='SUBMIT', font=('Arial', 20, 'bold'), command=enterButtonClicked)
@@ -51,8 +66,19 @@ passwordEntry.grid(row=1, column=0)
 enterButton.grid(row=2, column=0)
 
 
-nameDisplay = Label(homepage, text=f"Hello", font=('Arial', 12, 'bold'))
+
+nameDisplay = Label(homepage, text=f"Hello {UserBankObject.name}", font=('Arial', 18, 'bold'))
+balanceDisplay = Label(homepage, text=f"Balance: {UserBankObject.balance}", font=('Arial', 18, 'bold'))
+depositButton = Button(homepage, text="Deposit", command= deposit)
+withdrawButton = Button(homepage, text="Withdraw", command= withdraw)
+amountEntry = Entry(homepage)
+amountEntry.insert(0, "Enter amount here")
+
 nameDisplay.grid(row=0, column=0)
+balanceDisplay.grid(row=0, column=1)
+amountEntry.grid(row=1, column=0)
+withdrawButton.grid(row=2, column=0)
+depositButton.grid(row=2, column=1)
 
 
 root.mainloop()
